030a495f256a2687fedbdbeafef0e7786547c8bf
Hectic Envek <burningremedy@gmail.com>
2020-06-27 14:10:15 -0500

    Complete initial ListContact Component

---
+++ src/ListContacts.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react'
+
+class ListContacts extends Component {
+
+    
+    render(){
+        console.log(this.props)
+        return (
+            <ol className='contact-list'>
+                {this.props.contacts.map(contact => (
+                    <li key={contact.id} className="contact-list-item">
+                        <div className="contact-avatar"
+                        style={{
+                            backgroundImage: `url(${contact.avatarURL})`
+                        }}></div>
+                        <div className="contact-details">
+                            <p>{contact.name}</p>
+                            <p>{contact.handle}</p>
+                        </div>
+                        <button classname="contact-remove">
+                            Remove
+                        </button>
+                    </li>
+                ))}
+            </ol>
+        )
+    }
+
+}
+
+export default ListContacts
