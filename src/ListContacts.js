57977c79deb5fec6292dab58c9963dda3c269701
Hectic Envek <burningremedy@gmail.com>
2020-06-27 17:06:15 -0500

    Convert ListContacts from class to function

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,31 +1,25 @@
-import React, {Component} from 'react'
+import React from 'react'
 
-class ListContacts extends Component {
-
-    
-    render(){
-        console.log(this.props)
-        return (
-            <ol className='contact-list'>
-                {this.props.contacts.map(contact => (
-                    <li key={contact.id} className="contact-list-item">
-                        <div className="contact-avatar"
-                        style={{
-                            backgroundImage: `url(${contact.avatarURL})`
-                        }}></div>
-                        <div className="contact-details">
-                            <p>{contact.name}</p>
-                            <p>{contact.handle}</p>
-                        </div>
-                        <button classname="contact-remove">
-                            Remove
-                        </button>
-                    </li>
-                ))}
-            </ol>
-        )
-    }
-
+function ListContacts(props){
+    return(
+        <ol className='contact-list'>
+            {props.contacts.map(contact => (
+                <li key={contact.id} className="contact-list-item">
+                    <div className="contact-avatar"
+                    style={{
+                        backgroundImage: `url(${contact.avatarURL})`
+                    }}></div>
+                    <div className="contact-details">
+                        <p>{contact.name}</p>
+                        <p>{contact.handle}</p>
+                    </div>
+                    <button classname="contact-remove">
+                        Remove
+                    </button>
+                </li>
+            ))}
+        </ol>
+    )
 }
 
 export default ListContacts
