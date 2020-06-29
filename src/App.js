90c127d7ba5b35b411e65cecdac7daa9915ba7b7
Hectic Envek <burningremedy@gmail.com>
2020-06-29 17:48:23 -0500

    connect to contact api instead of local contacts

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts'
+import CreateContact from './CreateContact'
+import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
 
   state = {
-    contacts: [
-      {
-        "id": "karen",
-        "name": "Karen Isgrigg",
-        "handle": "karen_isgrigg",
-        "avatarURL": "http://localhost:5001/karen.jpg"
-      },
-      {
-        "id": "richard",
-        "name": "Richard Kalehoff",
-        "handle": "richardkalehoff",
-        "avatarURL": "http://localhost:5001/richard.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "handle": "tylermcginnis",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      }
-    ]
+    contacts: [],
+    screen : 'list'
+  }
+
+  componentDidMount(){
+    ContactsAPI.getAll()
+    .then((contacts)=>{
+      this.setState({contacts})
+    })
   }
 
   removeContact = (deleteThisContact) => {
@@ -32,12 +23,19 @@ class App extends Component {
         return contact.id !== deleteThisContact.id
       })
     }))
+    ContactsAPI.remove(deleteThisContact)
   }
 
   render() {
     return (
       <div>
-        <ListContacts onDelete={this.removeContact} contacts={this.state.contacts} />
+        {this.state.screen === 'list' && (<ListContacts onDelete={this.removeContact} contacts={this.state.contacts}
+          onNavigate={()=>{
+            this.setState(()=>({
+              screen:'create'
+            }))
+          }}/> )}
+        {this.state.screen === 'create' && (<CreateContact />)}
       </div>
     );
   }
